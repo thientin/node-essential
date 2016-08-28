ccaed3ebdbb780b79caaf4f6bb8ff8814c957cf2
Thien Tin <imthientin@gmail.com>
2016-08-28 19:20:22 +0700

    event emitter init

---
+++ event_emitter.js
@@ -0,0 +1,10 @@
+
+var events = require('events');
+
+var emitter = new events.EventEmitter();
+
+emitter.on('customEvent', function (message, status) {
+    console.log(`${status}: ${message}`);
+});
+
+emitter.emit('customEvent', "Hello There", 200);
